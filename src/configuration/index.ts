28ec6837b47c20f785991a6067732bfbb15fd547
zhangsgithub04 <ssen_zhang@hotmail.com>
2024-04-21 14:27:45 -0400

    Update index.ts

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -1,4 +1,4 @@
 export const DATA_BASE_CONFIGURATION = {
-  mongoConnectionString: process.env.CLEAN_NEST_MONGO_CONNECTION_STRING as string,
-  //mongoConnectionString: 'mongodb+srv://<uname>:<password>@newdev.ob1jxog.mongodb.net/bdx?retryWrites=true&w=majority&appName=newdev'
+  //mongoConnectionString: process.env.CLEAN_NEST_MONGO_CONNECTION_STRING as string,
+  mongoConnectionString string: 'mongodb+srv://<uname>:<password>@newdev.ob1jxog.mongodb.net/bdx?retryWrites=true&w=majority&appName=newdev'
 };

dc48bc059837cf401540beac5c8f0a3610c374e4
Wenxin Mao <maowenxin72@yahoo.com>
2024-04-22 06:47:05 -0500

    mongodb

--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -1,4 +1,4 @@
 export const DATA_BASE_CONFIGURATION = {
   //mongoConnectionString: process.env.CLEAN_NEST_MONGO_CONNECTION_STRING as string,
-   mongoConnectionString: 'mongodb+srv://stephen2:888888@newdev.ob1jxog.mongodb.net/bdx?retryWrites=true&w=majority&appName=newdev'
+   mongoConnectionString: 'mongodb+srv://maowenxin72:testmongodb@cluster0.cldfgqp.mongodb.net/'
 };
